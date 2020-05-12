e1519dab6023e331e18a396e82e37d383bdf296e
Ednilton Junior <jrjunior_cg@live.com>
2020-05-12 04:43:34 -0300

    Listando Agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Appointment[] = [];
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentsInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentsInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentsInSameDate) {
@@ -26,14 +23,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-
-  appointments.push(appointment);
-
+  const appointment = appointmentsRepository.create(provider, parsedDate);
   return response.json(appointment);
 });
 
